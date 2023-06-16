8c9ba811248400153dbf9dec8c42e595e39d4989
Geovana <geovana.so@aluno.ufop.edu.br>
2023-06-16 12:08:26 -0300

    injetando um serviço em outro serviço

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CursosService } from './cursos/cursos.service';
+//import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,8 @@ import { CursosModule } from './cursos/cursos.module';
     CursosModule,
     CriarCursoModule
   ],
+  providers: [LogService],
+
   //providers: [CursosService],
   bootstrap: [AppComponent]
 })
