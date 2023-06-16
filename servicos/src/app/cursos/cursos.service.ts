d4c7f90d65fc4c9dc27f4319e14ede791c0ddf52
Geovana <geovana.so@aluno.ufop.edu.br>
2023-06-16 11:23:53 -0300

    escopo de serviços: singleton e múltiplas instâncias

--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class CursosService {
 
+    private cursos: string[] = ['Angular', 'Java', 'Phonegap'];
+
+    constructor(){
+        console.log('nomejk')
+    }
+
     getCursos(){
-        return ['Angular', 'Java', 'Phonegap'];
+        return this.cursos;
+    }
+
+    addCurso(curso: string){
+        this.cursos.push(curso);
     }
 }
