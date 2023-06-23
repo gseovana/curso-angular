211870a8d1da1ba37bd8044d78b79a5206c881ee
Aluno <geovanacftds1@gmail.com>
2023-06-23 11:46:50 -0300

    criando módulo de rotas de funcionalidade

--- rotas/src/app/cursos/cursos.module.ts
+++ rotas/src/app/cursos/cursos.module.ts
@@ -5,12 +5,14 @@ import { CursosComponent } from "./cursos.component";
 import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
 import { CursoNaoEncontradoComponent } from "./curso-nao-encontrado/curso-nao-encontrado.component";
 import { CursosService } from "./cursos.service";
-import { RouterModule } from "@angular/router";
+import { CursosRoutingModule } from "./cursos-routing.module";
+//import { RouterModule } from "@angular/router";
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        //RouterModule,
+        CursosRoutingModule
     ],
     exports: [],
     declarations: [
