8c1b33f636479a38b16d1e28ae79225329e495c4
Aluno <geovanacftds1@gmail.com>
2023-06-23 10:35:56 -0300

    escutando mudanças nos parâmetros de roteamento

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -6,20 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     routing
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
