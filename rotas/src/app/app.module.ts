255f9df55cd3b767265f8cdd97f94235b7a6ba4c
Aluno <geovanacftds1@gmail.com>
2023-06-23 12:06:06 -0300

    rotas filhas

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosComponent } from './alunos/alunos.component';
+import { AlunosModule } from './alunos/alunos.module';
 
 //import { CursosComponent } from './cursos/cursos.component';
 //import { CursosService } from './cursos/cursos.service';
@@ -24,6 +26,7 @@ import { CursosModule } from './cursos/cursos.module';
   imports: [
     BrowserModule,
     CursosModule,
+    AlunosModule,
     AppRoutingModule
   ],
  // providers: [CursosService],
