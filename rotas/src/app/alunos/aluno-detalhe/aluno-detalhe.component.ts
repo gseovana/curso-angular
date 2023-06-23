d879b89a7084726efe96c2f3a8565d24660d0045
Aluno <geovanacftds1@gmail.com>
2023-06-23 12:56:01 -0300

    telas rotas filhas incompleto

--- rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-detalhe',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./aluno-detalhe.component.css']
 })
 export class AlunoDetalheComponent {
+  aluno: any;
+  inscricao: Subscription | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private alunosService: AlunosService
+  ){}
+
+  ngOnInit(){
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+
+        this.aluno = this.alunosService.getAluno(id);
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao?.unsubscribe();
+  }
 }
