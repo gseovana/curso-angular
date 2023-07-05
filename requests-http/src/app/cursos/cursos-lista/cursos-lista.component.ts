c176432e0b4c794169c9107b7bbc83542eb01017
Aluno <geovanacftds1@gmail.com>
2023-07-05 18:55:09 -0300

    criando projeto http & configurando ambiente

---
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos-lista',
+  templateUrl: './cursos-lista.component.html',
+  styles: [
+  ]
+})
+export class CursosListaComponent {
+
+}
