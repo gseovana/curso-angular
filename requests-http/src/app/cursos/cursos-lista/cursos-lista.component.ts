afb399b4a9c4048eb3f6ee03bd44478839260e31
Aluno <geovanacftds1@gmail.com>
2023-07-06 10:38:46 -0300

    captura de erros com async

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from '../curso';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable, Subject, catchError } from 'rxjs';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -14,13 +14,63 @@ export class CursosListaComponent {
   //cursos: Curso[];
 
   cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService){}
 
   ngOnInit(){
-   // this.service.list()
-   //   .subscribe(dados => this.cursos = dados);
+   /* this.service.list()
+     .subscribe(dados => this.cursos = dados);
 
-   this.cursos$ = this.service.list();
+   this.cursos$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return EMPTY;
+      })
+    ) */
+
+    this.onRefresh();
+  }
+
+  onRefresh(){
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return EMPTY;
+      })
+    );
+
+    //DEPRECATED 
+    /* this.service.list().pipe(
+      catchError(error => EMPTY)
+    )
+    .subscribe(
+      (dados: any) => {
+        console.log(dados);
+      },
+      error => console.error(error),
+      () => console.log('Observable completo!')
+    );*/ 
+
+    this.service.list().pipe(
+      catchError(error => EMPTY)
+    )
+    .subscribe({
+      next: dados => {
+        console.log(dados);
+      },
+      error: error => {
+        console.error(error);
+      },
+      complete: () => {
+        console.log('Observable completo!');
+      }
+    });
+    
+    
   }
 }
